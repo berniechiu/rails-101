2c85195e6914a6b4a4290ad741fb52098ce237ef
Bernie Chiu <bernie_chiu@hotmail.com>
2015-03-03 15:06:50 +0800

    Allow update for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
   def post_params
     params.require(:post).permit(:content)
   end
