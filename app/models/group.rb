bb7d565790e2426730bf917c0e690511d105b69d
Bernie Chiu <bernie_chiu@hotmail.com>
2015-03-03 20:41:08 +0800

    Authorize owners own modification on edit and delete

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,8 @@ class Group < ActiveRecord::Base
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
   validates :title, presence: true
+
+  def editable_by?(user)
+    user && user == owner
+  end
 end
