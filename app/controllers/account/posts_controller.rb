80a8844f500ba6eefc50655d8da607189f84a2ac
Bernie Chiu <bernie_chiu@hotmail.com>
2015-03-04 15:11:35 +0800

    Show groups based on posts count

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,5 +1,5 @@
 class Account::PostsController < ApplicationController
   def index
-    @posts = current_user.posts.order("updated_at DESC")
+    @posts = current_user.posts.order("posts_count DESC")
   end
 end
