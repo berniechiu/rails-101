1ea1b3dab481ad81c73237dae019129541ff7652
Bernie Chiu <bernie_chiu@hotmail.com>
2015-03-04 10:11:45 +0800

    Add user id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303104000) do
+ActiveRecord::Schema.define(version: 20150304021005) do
 
   create_table "groups", force: true do |t|
     t.string   "title"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20150303104000) do
     t.integer  "group_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
