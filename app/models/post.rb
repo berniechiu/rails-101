984acab096a81dd198de4751044967f690d29d74
Bernie Chiu <bernie_chiu@hotmail.com>
2015-03-04 16:21:20 +0800

    Update recent posts to scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :group, counter_cache: true
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
+  scope :recent, -> { order("updated_at DESC") }
+
   validates :content, presence: true
 
   def editable_by?(user)
